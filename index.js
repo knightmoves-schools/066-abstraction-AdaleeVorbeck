efdc381edc03c89ba18bdb65abaaf626a00e6123
AdaleeVorbeck <adavor0529@students.maquoketaschools.org>
2026-01-16 18:38:32 +0000

    AND YOU CAN GET WHAT YOU WANT OR YOU CAN JUST GET OLD

--- index.js
+++ index.js
@@ -3,12 +3,22 @@ class TaxCalculator{
         #rate = 15;
         state = 'TX';
         #exempt = false;
-    
+        #calculateExempt(value){
+            return this.#rate * value;
+        }
+        #calculateNonExempt(value){
+            return this.#rate * value;
+        }
+
     calculate(){
-        if(this.exempt){
+        if(this.#exempt){
             return this.#calculateExempt(1.37);
         }else{
             return this.#calculateNonExempt(5.72);
         }
     }
 }
+
+//    ? the index.js file should hide the `rate`
+//    ? the index.js file should hide the `calculateExempt` method
+//    ? the index.js file should hide the `calculateNonExempt` method
